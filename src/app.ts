1a16c051a78ff2edd5ee8c22c6ab99661812862b
Richard Chou <konami99@hotmail.com>
2023-08-30 21:27:17 +1000

    updated lambda

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
+import { S3 } from "aws-sdk";
+
+const bucketName = process.env.DestinationBucketName as string;
+
+const s3 = new S3();
+
 export const lambdaHandler = async (event: any): Promise<any> => {
   try {
+		await s3.putObject({
+			Bucket: bucketName,
+			Key: 'index.html',
+			Body: '<html><head></head><body>this is a test 123</body></html>'
+		}).promise();
+				
 		return {
 			statusCode: 200,
 			body: JSON.stringify({
